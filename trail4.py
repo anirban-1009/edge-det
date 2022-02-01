03a5cc79e3da58b221a6ec66a2e5786e03f86b30
anirban-1009 <anirban9293@gmail.com>
2022-02-01 15:57:49 +0530

    just update

---
+++ trail4.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def process(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    grad_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+    grad_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
+
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+
+    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    return grad
+
